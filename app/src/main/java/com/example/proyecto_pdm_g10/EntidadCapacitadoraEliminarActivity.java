e67a67ef27a83aba216c270f9fd2415df94ce88c
Miguel <miguel.galdamez@hotmail.com>
2021-05-04 17:33:38 -0600

    creando los archivos

---
+++ app/src/main/java/com/example/proyecto_pdm_g10/EntidadCapacitadoraEliminarActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_pdm_g10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntidadCapacitadoraEliminarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entidad_capacitadora_eliminar);
+    }
+}
